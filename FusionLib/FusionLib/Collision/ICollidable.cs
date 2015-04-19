88cbffd915e835d6f6e73a922e79153bcf833bed
Evan McIntire <mcintire.evan@gmail.com>
2015-04-19 00:00:21 -0400

    Add Entity class and ICollidable interface
    Entity serves as a base for other entities(character, enemy, etc.)
    ICollidable provides methods that will be used to perform collision
    detection

---
+++ FusionLib/FusionLib/Collision/ICollidable.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace FusionLib.Collision
+{
+    /// <summary>
+    /// Interface for collision detection, provides methods used by the collision detector
+    /// </summary>
+    public interface ICollidable
+    {
+        void OnCollision(ICollidable other);
+        Rectangle GetHitbox();
+    }
+}
---
+++ FusionLib/FusionLib/Entities/Entity.cs
@@ -0,0 +1,35 @@
+﻿using FusionLib.Collision;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FusionLib.Entities
+{
+    /// <summary>
+    /// Serves as a base for other entities
+    /// </summary>
+    public class Entity : ICollidable
+    {
+        protected Texture2D texture;
+        protected Vector2 pos;
+        protected Rectangle hitbox;
+
+        public Entity(Texture2D texture, Vector2 pos)
+            : this(texture, pos, new Rectangle((int)pos.X, (int)pos.Y, texture.Width, texture.Height)) { }
+
+        public Entity(Texture2D texture, Vector2 pos, Rectangle hitbox)
+        {
+            this.texture = texture;
+            this.pos = pos;
+            this.hitbox = hitbox;
+        }
+
+        public virtual void OnCollision(ICollidable other) { }
+        public virtual Rectangle GetHitbox()
+        {
+            return this.hitbox;
+        }
+
+        public virtual void Update(GameTime gameTime) { }
+        public virtual void Draw(GameTime gameTime) { }
+    }
+}
