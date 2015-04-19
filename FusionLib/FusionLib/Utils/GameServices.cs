a40b56830e60bc147ca8a1206d32f575de830749
Evan McIntire <mcintire.evan@gmail.com>
2015-04-19 15:02:09 -0400

    Add GameServices and Performance classes
    GameServices is a static reference to a GameServicesContainer that can
    contain things such as a GraphicsDevice or a ContentManager
    
    Performance has methods to check FPS and RAM usage

---
+++ FusionLib/FusionLib/Utils/GameServices.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace FusionLib.Utils
+{
+    /// <summary>
+    /// A wrapper class to serve as a static reference to a GameServiceContainer
+    /// </summary>
+    public static class GameServices
+    {
+        private static GameServiceContainer container;
+        public static GameServiceContainer Instance
+        {
+            get
+            {
+                if (container == null)
+                {
+                    container = new GameServiceContainer();
+                }
+                return container;
+            }
+        }
+
+        /// <summary>
+        /// Gets the service of type T
+        /// </summary>
+        /// <typeparam name="T">Type of service to get</typeparam>
+        /// <returns></returns>
+        public static T GetService<T>()
+        {
+            return (T)Instance.GetService(typeof(T));
+        }
+
+        /// <summary>
+        /// Adds a service 'service' of type T
+        /// </summary>
+        /// <typeparam name="T">Type of service</typeparam>
+        /// <param name="service">Service</param>
+        public static void AddService<T>(T service)
+        {
+            Instance.AddService(typeof(T), service);
+        }
+
+        /// <summary>
+        /// Removes the service stored for type T
+        /// </summary>
+        /// <typeparam name="T">Type to remove</typeparam>
+        public static void RemoveService<T>()
+        {
+            Instance.RemoveService(typeof(T));
+        }
+    }
+}
---
+++ FusionLib/FusionLib/Utils/Performance.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Diagnostics;
+
+namespace FusionLib.Utils
+{
+    /// <summary>
+    /// A utils class containing things related to performance such as memory usage and framerate.
+    /// </summary>
+    public static class Performance
+    {
+        /// <summary>
+        /// Get the current framerate
+        /// </summary>
+        /// <param name="g"></param>
+        /// <returns></returns>
+        public static double GetFramerate(GameTime g)
+        {
+            return 1 / g.ElapsedGameTime.TotalSeconds;
+        }
+
+        /// <summary>
+        /// Get the current memory usage
+        /// </summary>
+        /// <param name="suffix">Suffix of byte amound (b, kb, mb)</param>
+        /// <returns></returns>
+        public static double GetMemoryUsage(String suffix)
+        {
+            suffix = suffix.ToLower();
+            Process proc = Process.GetCurrentProcess();
+            double mem = proc.PrivateMemorySize64;
+            if (suffix.Equals("b"))
+            {
+                return mem;
+            }
+            else if (suffix.Equals("kb"))
+            {
+                return mem / 1024;
+            }
+            else if (suffix.Equals("mb"))
+            {
+                return (mem / 1024) / 1024;
+            }
+            else
+            {
+                return proc.PrivateMemorySize64;
+            }
+        }
+    }
+}
