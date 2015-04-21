318b8a4157b53831e7196671f3f8ede918ca64a4
Evan McIntire <mcintire.evan@gmail.com>
2015-04-20 23:16:56 -0400

    Add CollidableRectangle class

---
+++ FusionLib/FusionLib/Shapes/CollidableRectangle.cs
@@ -0,0 +1,24 @@
+﻿using FusionLib.Collision;
+using Microsoft.Xna.Framework;
+
+namespace FusionLib.Shapes
+{
+    public class CollidableRectangle : ICollidable
+    {
+        private Rectangle rectangle;
+
+        public CollidableRectangle(Rectangle rectangle)
+        {
+            this.rectangle = rectangle;
+        }
+
+        public Rectangle GetHitbox()
+        {
+            return rectangle;
+        }
+
+        public void OnCollision(ICollidable o)
+        {
+        }
+    }
+}
