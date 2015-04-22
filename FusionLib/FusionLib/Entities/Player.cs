927135507857a5e01353255599d381b82af94b10
Evan McIntire <mcintire.evan@gmail.com>
2015-04-21 23:06:52 -0400

    Add start of player class

---
+++ FusionLib/FusionLib/Entities/Player.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FusionLib.Entities
+{
+    public class Player : Entity
+    {
+        protected Vector2 velocity;
+
+        public Player(Texture2D texture, Vector2 pos)
+            : base(texture, pos)
+        {
+            velocity = Vector2.Zero;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, hitbox, Color.White);
+        }
+    }
+}
